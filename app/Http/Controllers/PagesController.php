a64d0c058922a95c051a3c865317e0c1abdfe472
zura <zurakiziria@gmail.com>
2018-05-24 14:29:13 +0400

    First commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index(){
+        
+        return view('pages.main');
+    }
+    
+    public function about(){
+         return view('pages.about');
+    }
+    
+    public function contact(){
+        return view('pages.contact');
+    }
+}
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+ ABOUT Content Body
+@endsection
